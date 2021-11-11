1f1f33fb98c986c5fe29c889c0d934919328fdae
Sayan <88580652+AkiyoSayan@users.noreply.github.com>
2021-11-11 15:11:15 +0530

    Update config.py

--- Cutiepii_Robot/config.py
+++ Cutiepii_Robot/config.py
@@ -37,7 +37,7 @@ class Config(object):
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules # its "URI" and not "URL" as herok and similar ones only accept it as such
+    DB_URL = "something://somewhat:user@hosturl:port/databasename"  # needed for any database modules # its "URI" and not "URL" as herok and similar ones only accept it as such
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
